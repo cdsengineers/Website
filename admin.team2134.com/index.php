1f56101931e9f0cfd275e44c05b2bbe3cf8f129c
Alex Slaughter <alex@alexslaughter.com>
2012-07-13 11:07:37 -0700

    init commit of admin site

---
+++ admin.team2134.com/index.php
@@ -0,0 +1,17 @@
+<?php
+	require_once("team2134.com.inc");
+	require_once("functions.inc");
+
+	$ca = new CentralAuth();
+	$ca->protect("admin");
+?>
+
+<html>
+	<head>
+		<title>team2134.com admin</title>
+	</head>
+	
+	<body>
+		
+	</body>
+</html>
