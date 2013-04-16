0156e490c0edd97c776999bf1c4a4f83db8a1624
Zi Yang Lai <zy.lai@att.net>
2013-04-16 10:08:09 -0700

    Update index.php
    test

--- test.team2134.com/index.php
+++ test.team2134.com/index.php
@@ -7,13 +7,7 @@
 
 <font size="5">
 
-We are FIRST Team 2134 from Corona del Sol High School located in Tempe, Arizona. Our mascot is the Pirates and starting last year, we have teamed up with McClintock High School.
-
-This year will be our 6th year being a member of FIRST robotics. Our team embodies and embraces the concepts of FIRST and of course gracious professionalism. It is our goal this year to both compete in the 2012 FRC competition, as well as mentor a FLL team. We believe that through community involvement and hard work, we can spread the concepts of FIRST and inspire those in the next generation to become scientists and engineers and hopefully cultivate a healthy interest in science and technology.
-
-If you are a parent and you child is interested in either FIRST, joining our team, or you just want more information. Feel free to just send us an email at cdsengineers@team2134.com for more information and meeting times. You can also check out our Agenda for event and meeting updates.
-
-Sponsors! If you are interested in sponsoring our team, please visit our Sponsors page for more information and where you may contact us.
+test
 
 </font>
 </body>
